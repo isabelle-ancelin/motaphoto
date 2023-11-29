6fe823cd3c99aca980d1083b637b95d69701593a
Isabelle.Ancelin <77393135+isabelle-ancelin@users.noreply.github.com>
2023-11-29 11:58:06 +0100

    mise en plage de la structure du contenu

--- wp-content/themes/motaphoto/functions.php
+++ wp-content/themes/motaphoto/functions.php
@@ -7,8 +7,8 @@ function theme_enqueue_styles()
     wp_enqueue_style("motaphoto-style", get_stylesheet_directory_uri() . "/css/theme.css");
 
     // Charger les polices Space Mono et Poppins
-    //wp_enqueue_style("space-mono-font", get_stylesheet_directory_uri() . "/fonts/Space-Mono/stylesheet.css", array(), );
-    //wp_enqueue_style("poppins-font", get_stylesheet_directory_uri() . "/fonts/Poppins/stylesheet.css", array(), );
+    wp_enqueue_style("space-mono-font", get_stylesheet_directory_uri() . "/fonts/Space-Mono/stylesheet.css", array(), );
+    wp_enqueue_style("poppins-font", get_stylesheet_directory_uri() . "/fonts/Poppins/stylesheet.css", array(), );
 
     wp_enqueue_script("script-js", get_stylesheet_directory_uri() . "/js/script.js");
 }
